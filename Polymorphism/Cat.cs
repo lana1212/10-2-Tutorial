c0ae79127875b94c4842e33b2ebe3ffaa3454be7
Lana Karazoun <lkarazoun@usf.edu>
2021-03-22 17:04:32 -0400

    Add project files.

---
+++ Polymorphism/Cat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms; 
+
+namespace Polymorphism
+{
+    class Cat : Animal
+    {
+        //field
+        private string _name;
+
+        //constructor
+        public Cat (string name)
+            : base("Cat")
+        {
+            _name = name;
+        }
+
+        //name property
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        //makesound method
+        public override void MakeSound()
+        {
+            MessageBox.Show("Meow");
+        }
+    }
+}
